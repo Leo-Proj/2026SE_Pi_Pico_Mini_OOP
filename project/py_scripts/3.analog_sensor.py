057aa62cb021f3e418736704f958d3af3ac4d9d9
Leo-Proj <leo.projceski@education.nsw.gov.au>
2025-05-29 14:59:23 +1000

    3 and 4 complete

--- project/py_scripts/3.analog_sensor.py
+++ project/py_scripts/3.analog_sensor.py
@@ -1,4 +1,4 @@
-from machine import Pin
+from machine import Pin, ADC
 from time import sleep
 
 sleep(0.1)
@@ -6,6 +6,21 @@ sleep(0.1)
 led_pin = 25
 led2_pin = 15
 data_pin = 13
+analog_data_pin = 26
 
 led = Pin(led_pin, Pin.OUT)
-led2 = Pin(led2_pin)
+led2 = Pin(led2_pin, Pin.OUT)
+
+data = Pin(data_pin, Pin.IN)
+
+adc_value = ADC(Pin(analog_data_pin))
+
+while True:
+    if data.value() == 1:
+        led.value(True)
+        led2.value(False)
+    else:
+        led.value(False)
+        led2.value(True)
+    print(print(f"analog: {adc_value.read_u16()}"))
+    sleep(0.1)
