8e715bd7f05519b5a6d97340a86e44d62bfa7352
Leo-Proj <leo.projceski@education.nsw.gov.au>
2025-06-17 13:24:16 +1000

    fixed pedestrian button code

--- project/lib/pedestrian_button.py
+++ project/lib/pedestrian_button.py
@@ -16,40 +16,47 @@ class Pedestrian_Button(Pin):
         super().__init__(pin, Pin.IN, Pin.PULL_DOWN)
         self.__debug = debug
         self.__pin = pin
-        self.__last_press = 0
+        self.__last_pressed = 0
         self.__pedestrian_waiting = False
-        self.button_state
+        self.irq(
+            trigger=Pin.IRQ_RISING, handler=self.callback
+        )
 
-    @property
-    '''
-    Get the state of the button, not whether the button is being pressed or not pressed, but if the button has been pressed
-    '''
-    def button_state(self):
-        if self.__debug:
-            print(f"Button connected to Pin {self.__pin} is {'WAITING' if self.__pedestrian_waiting else 'NOT WAITING'}")
-        return self.__pedestrian_waiting
-    
-    @button_state.setter
-    '''
-    Resets the pedestrian_waiting 
-    '''
-    def button_state(self, value):
-        self.__pedestrian_waiting = value
-        if self.__debug:
-            print(f"Button state on Pin {self.__pin} set to {value}")
-
-    class Pedestrian_Button(Pin):
-    '''
-    Records the state change without interupting current processes
-    '''
-        def __init__(self, pin, debug):
-            ...
-            self.irq(trigger=Pin.IRQ_RISING, handler=self.callback)
+    def button_state(self, value=None):
+        if value is None:
+            # Getter
+            if self.__debug:
+                print(f"Button connected to Pin {self.__pin} is {'WAITING' if self.__pedestrian_waiting else 'NOT WAITING'}")
+            return self.__pedestrian_waiting
+        else:
+            self.__pedestrian_waiting = bool(value)
+            if self.__debug:
+                print(f"Button state on Pin {self.__pin} set to {self.__pedestrian_waiting}")
 
     def callback(self, pin):
         current_time = time.ticks_ms()
-        if (time.ticks_diff(current_time, self.__last_pressed) > 200):
+        if (time.ticks_diff(current_time, self.__last_pressed) > 200): 
             self.__last_pressed = current_time
             self.__pedestrian_waiting = True
             if self.__debug:
                 print(f"Button pressed on Pin {self.__pin} at {current_time}ms")
+
+
+
+
+
+
+'''
+    Get the state of the button, not whether the button is being pressed or not pressed, but if the button has been pressed
+    '''
+
+
+
+    '''
+    Resets the pedestrian_waiting 
+    '''
+
+
+    '''
+    Records the state change without interupting current processes
+    '''
